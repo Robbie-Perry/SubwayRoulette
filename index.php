2a8f4bf18b9d04ddedc7f11bfa7b401aff7e4214
Robbie Perry <robbieperry22@gmail.com>
2017-10-18 14:37:36 -0700

    Removed old html

--- index.php
+++ index.php
@@ -6,20 +6,26 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
-<body>
-<div class="row">
-    <div class="col-xs-10 bg-success col-xs-offset-1">
+<body class="subway-2-bg">
+<div class="row subway-2">
+    <div class="col-xs-10 bg-success col-xs-offset-1 subway-1-bg">
         <div class="row">
-            <div class="col-xs-8 col-xs-offset-2">
-                <h1>Subway Roulette</h1>
+            <div class="col-xs-8 col-xs-offset-2 container">
+                <div class="jumbotron">
+                    <h1><span class="subway-1">Subway</span> <span class="subway-2">Roulette</span></h1>
+                </div>
             </div>
         </div>
+        <div class="col-xs-10 col-xs-offset-1 bg-warning content">
 <?php
-$breads = array("9-Grain Wheat", "Multi-grain Flatbread", "Italian", "Italian Herbs and cheese", "Flatbread");
+$breads = array("9-Grain Wheat", "Multi-grain Flat", "Italian", "Italian Herbs and cheese", "Flat");
 $sauces = array("Chipotle Southwest", "Light Mayo", "Mayo", "Ranch",
     "Oil", "Subway Vinaigrette", "Mustard", "Vinegar", "Sweet Onion",
     "BBQ", "Creamy italian", "Golden italian", "Honey Mustard", "Savory Caesar" , "Sriracha", "Tzatziki Cucumber");
 $veggies = array("Cucumbers", "Green Peppers", "Lettuce", "Red Onions", "Spinach", "Tomatoes", "Banana Peppers", "Jalapenos", "Black Olives", "Pickles");
+$sandwiches = array("Black Forest Ham", "Chicken and Bacon Ranch Melt", "Cold Cut Combo", "Italian BMT", "Meatball Marinara", "Oven Roasted Chicken",
+    "Roast Beef", "Rotisserie-Style Chicken", "Spicy Italian", "Steak and Cheese", "Subway Club", "Sweet Onion Chicken Teriyaki",
+    "Classic Tuna", "Turkey Breast");
 
 if (!isset($_GET['numSauce'])) {
     echo '<form action="?" method="get">
@@ -29,6 +35,7 @@ if (!isset($_GET['numSauce'])) {
 </form>';
 
 } else {
+    $sandwichChoice = $sandwiches[rand(0, count($sandwiches) - 1)];
     $breadChoice = $breads[rand(0, 4)];
     $sauceChoices = array_fill(0, $_GET['numSauce'], "");
     $veggieChoices = array_fill(0, $_GET['numVeggies'], "");
@@ -48,12 +55,13 @@ if (!isset($_GET['numSauce'])) {
         }
         $veggieChoices[$i] = $addVeggie;
     }
-    echo "Your Subway sandwich will have: <br />";
+    echo "Your $sandwichChoice sandwich will have: <br />";
     echo "$breadChoice bread,<br />";
     var_dump($sauceChoices);
     var_dump($veggieChoices);
 }
 ?>
+        </div>
     </div>
 </div>
 </body>
